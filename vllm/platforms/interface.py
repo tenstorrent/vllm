0b8f418aee5a8b968fe7a6abba0aaf17a6da826e
Salar Hosseini <skhorasgani@tenstorrent.com>
2024-09-26 21:02:17 +0000

    Add TTPlatform to remove vllm._C missing warning
    Signed-off-by: Salar Hosseini <skhorasgani@tenstorrent.com>

--- vllm/platforms/interface.py
+++ vllm/platforms/interface.py
@@ -8,6 +8,7 @@ class PlatformEnum(enum.Enum):
     CUDA = enum.auto()
     ROCM = enum.auto()
     TPU = enum.auto()
+    TT = enum.auto()
     UNSPECIFIED = enum.auto()
 
 
@@ -22,6 +23,9 @@ class Platform:
 
     def is_tpu(self) -> bool:
         return self._enum == PlatformEnum.TPU
+    
+    def is_tt(self) -> bool:
+        return self._enum == PlatformEnum.TT
 
     @staticmethod
     def get_device_capability(device_id: int = 0) -> Tuple[int, int]:
