9f7ad5c0393b06b2552ac287e9a839d17df5db9a
Salar Hosseini <skhorasgani@tenstorrent.com>
2024-10-23 15:35:07 +0000

    async output proc wip
    Signed-off-by: Salar Hosseini <skhorasgani@tenstorrent.com>

--- vllm/config.py
+++ vllm/config.py
@@ -376,7 +376,7 @@ class ModelConfig:
 
         # Reminder: Please update docs/source/serving/compatibility_matrix.rst
         # If the feature combo become valid
-        if device_config.device_type not in ("cuda", "tpu", "xpu"):
+        if device_config.device_type not in ("cuda", "tpu", "xpu", "tt"):
             logger.warning(
                 "Async output processing is only supported for CUDA, TPU, XPU. "
                 "Disabling it for other platforms.")
