e0d9ec49d493a604b8039677a294554d8729c146
ssanjay <ssanjay@tenstorrent.com>
2025-07-18 16:03:59 -0400

    precommit fixes

--- vllm/worker/tt_worker.py
+++ vllm/worker/tt_worker.py
@@ -335,16 +335,20 @@ class TTWorker(LoRANotSupportedWorkerBase, LocalOrDistributedWorkerBase):
                 model_input = cast(TTModelInput, model_input)
                 self.cached_model_input = model_input
                 balance_tokens = []
-                for seq_group_metadata in execute_model_req.seq_group_metadata_list:
+                metadata_list = execute_model_req.seq_group_metadata_list
+                for seq_group_metadata in metadata_list:
                     seq_datas = list(seq_group_metadata.seq_data.values())
-                    assert len(
-                        seq_datas
-                    ) == 1, "Currently only supporting one sequence per request group"
-                    balance_tokens.append(seq_group_metadata.sampling_params.max_tokens - seq_datas[0].get_output_len())
+                    assert len(seq_datas) == 1, (
+                        "Currently only supporting one sequence per "
+                        "request group")
+                    max_tokens = seq_group_metadata.sampling_params.max_tokens
+                    balance_tokens.append(max_tokens -
+                                          seq_datas[0].get_output_len())
                 balance_tokens = max(balance_tokens)
                 worker_input = dataclasses.replace(
                     worker_input,
-                    num_steps=min(execute_model_req.num_lookahead_slots + 1, balance_tokens))
+                    num_steps=min(execute_model_req.num_lookahead_slots + 1,
+                                  balance_tokens))
             else:
                 assert self.cached_model_input is not None
                 model_input = self.cached_model_input
