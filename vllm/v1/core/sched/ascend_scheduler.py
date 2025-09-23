9134f368bb5168d371519e12d25b63abbf509c95
Salar <skhorasgani@tenstorrent.com>
2025-09-23 19:37:03 +0000

    Update license/copyright text for ascend scheduler
    Signed-off-by: Salar <skhorasgani@tenstorrent.com>

--- vllm/v1/core/sched/ascend_scheduler.py
+++ vllm/v1/core/sched/ascend_scheduler.py
@@ -1,22 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-# SPDX-FileCopyrightText: Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# SPDX-FileCopyrightText: Copyright 2025 Huawei Technologies Co., Ltd.
 #
-# Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# This file was copied from the vllm-ascend project (original at
+# This file was copied and modified from the vllm-ascend project (original at
 # https://github.com/vllm-project/vllm-ascend/blob/main/vllm_ascend/core/scheduler.py @ df0ec55)  # noqa: E501
 
 import time
