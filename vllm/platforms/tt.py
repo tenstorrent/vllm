265f350a6ffac6f5a0b008a8222aee7deddd5459
tchedaTT <tcheda@tenstorrent.com>
2025-11-18 17:03:58 +0000

    Trigger on seed and min_tokens, don't trigger on truncate

--- vllm/platforms/tt.py
+++ vllm/platforms/tt.py
@@ -190,7 +190,9 @@ class TTPlatform(Platform):
                 or sampling_params.logprobs is not None
                 or sampling_params.prompt_logprobs is not None
                 or sampling_params.logits_processors is not None
-                or sampling_params.truncate_prompt_tokens is not None
                 or sampling_params.guided_decoding is not None
                 or sampling_params.logit_bias is not None
-                or sampling_params.allowed_token_ids is not None)
+                or sampling_params.allowed_token_ids is not None
+                or sampling_params.seed is not None # Seeding not fully supported 
+                or sampling_params.min_tokens != 0)
+
