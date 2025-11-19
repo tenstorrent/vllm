7d25b00e0ca1e58a4b97ae53fe8a0bada073dbff
Ashai Reddy <arginuga@tenstorrent.com>
2025-11-19 17:38:00 +0000

    Generic condition for Qwen and mrope

--- vllm/worker/tt_model_runner.py
+++ vllm/worker/tt_model_runner.py
@@ -166,7 +166,7 @@ class TTModelRunner(ModelRunnerBase[TTModelInput]):
         self.cached_step_outputs: List[torch.Tensor] = [
         ]  # Only used for multi-step execution
 
-        self.request_specific_rope = 'Qwen2.5-VL' in self.model_config.model
+        self.request_specific_rope = bool(self.model_config.uses_mrope)
         if self.model_config.is_encoder_decoder or self.request_specific_rope:
             assert (
                 self.model_config.is_encoder_decoder
@@ -176,12 +176,6 @@ class TTModelRunner(ModelRunnerBase[TTModelInput]):
             # seq_id -> cached_req_data
             self.cached_req_data: Dict[int, Dict[str, Any]] = {}
             self.previous_seq_ids: Set[int] = set()
-
-        # Detect if the model has "mrope" rope_scaling type.
-        # mrope requires keep "rope_deltas" between prompt and decoding phases.
-        if self.model_config.uses_mrope:
-            assert ("TTModelRunner does not currently support models with "
-                    "mrope rope_scaling")
 
         vocab_size = self.model_config.get_vocab_size()
         self.logits_processor = LogitsProcessor(vocab_size,
