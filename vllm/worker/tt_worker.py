4f2eb54eeffb38c7969b47f67d0e1215f084e45c
Petar <pmilojevic@tenstorrent.com>
2026-01-25 16:12:18 +0000

    Added comment

--- vllm/worker/tt_worker.py
+++ vllm/worker/tt_worker.py
@@ -447,7 +447,7 @@ def get_num_available_blocks_tt(vllm_config: VllmConfig) -> int:
            or "Qwen2.5-14B" in model_config.model 
            or "gemma-3-4b" in model_config.model)
           and devices_per_dp_cache == 2 and is_wormhole):
-        # Qwen2.5-14B on N300
+        # Qwen2.5-14B and gemma3-4b on N300
         max_tokens_all_users = 65536
     elif ("Llama-3.2-90B" in model_config.model and devices_per_dp_cache == 8
           and is_wormhole):
