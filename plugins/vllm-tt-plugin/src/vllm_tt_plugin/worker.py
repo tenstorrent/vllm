30ba497a37120c2bb59134a14e3186c6c814bc3d
Veljko Cankovic <vcankovic@tenstorrent.com>
2026-08-07 15:02:21 +0200

    add BH-Galaxy to mesh_device mapping

--- plugins/vllm-tt-plugin/src/vllm_tt_plugin/worker.py
+++ plugins/vllm-tt-plugin/src/vllm_tt_plugin/worker.py
@@ -737,6 +737,7 @@ def get_mesh_grid(local_dp_rank=0):
         "P150x8": (1, 8),
         "P300x2": (1, 4),
         "TG": (8, 4),
+        "BH-Galaxy": (8, 4),
     }
     mesh_device_env = os.environ.get("MESH_DEVICE")
     if mesh_device_env is not None:
